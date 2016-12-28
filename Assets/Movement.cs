454cfaf1d04d483695b68c660a1ce6ca17d16737
Matteo  Ziff <matteoziff@Matteos-MacBook-Pro-2.local>
2016-12-27 22:31:36 -0500

    First Commit to this incredible game

---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed=8;
+	private Rigidbody rb;
+	private 
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+//			print ("triggered");
+			transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+		}
+
+	}
+}
